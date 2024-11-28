a59bf77d8a07c24b760b5a76a70526e4b98a2f70
Emanuel M. Diersmann <161858944+EmanuelMD09@users.noreply.github.com>
2024-11-28 07:11:27 -0300

    Add files via upload

---
+++ s.a/Cliente/comprarProduto1.js
@@ -0,0 +1,11 @@
+let carrinho = JSON.parse(localStorage.getItem('carrinho')) || [];
+
+function adicionarAoCarrinho(nomeProduto, precoProduto, imagemProduto) {
+    // Adiciona o produto ao carrinho
+    carrinho.push({ nome: nomeProduto, preco: precoProduto, imagem: imagemProduto });
+    alert("Produto adicionado ao carrinho");
+
+    // Atualiza o localStorage
+    localStorage.setItem('carrinho', JSON.stringify(carrinho));
+}
+
